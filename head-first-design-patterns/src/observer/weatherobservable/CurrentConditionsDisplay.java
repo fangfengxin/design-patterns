21e6793de04383379c8d4973035de270c4d2a284
fangfengxin <1208689216@qq.com>
2020-04-07 20:52:02 +0800

    Head First Design Patterns - Observer Patterns

---
+++ head-first-design-patterns/src/observer/weatherobservable/CurrentConditionsDisplay.java
@@ -0,0 +1,29 @@
+package observer.weatherobservable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+    private double temperature;
+    private double humidity;
+
+    public CurrentConditionsDisplay(Observable observable) {
+        observable.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        if (observable instanceof WeatherData) {
+            WeatherData weatherData = (WeatherData) observable;
+            this.temperature = weatherData.getTemperature();
+            this.humidity = weatherData.getHumidity();
+            display();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature
+                + "F degrees and " + humidity + "% humidity.");
+    }
+}
---
+++ head-first-design-patterns/src/observer/weatherobservable/StatisticsDisplay.java
@@ -0,0 +1,39 @@
+package observer.weatherobservable;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+	private double maxTemp = 0.0f;
+	private double minTemp = 200;
+	private double tempSum= 0.0f;
+	private int numReadings;
+
+	public StatisticsDisplay(Observable observable) {
+		observable.addObserver(this);
+	}
+
+	public void update(Observable observable, Object arg) {
+		if (observable instanceof WeatherData) {
+			WeatherData weatherData = (WeatherData)observable;
+			double temp = weatherData.getTemperature();
+			tempSum += temp;
+			numReadings++;
+
+			if (temp > maxTemp) {
+				maxTemp = temp;
+			}
+ 
+			if (temp < minTemp) {
+				minTemp = temp;
+			}
+
+			display();
+		}
+	}
+
+	public void display() {
+		System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+			+ "/" + maxTemp + "/" + minTemp);
+	}
+}
