ebeb7455557b1cc696eebf93b3fb1a8e04b8b693
flexia <fangfengxin98@163.com>
2020-04-29 10:27:14 +0800

    Head First Design Patterns - Command Pattern update Command classes

---
+++ head-first-design-patterns/src/command/party/StereoOffCommand.java
@@ -0,0 +1,22 @@
+package command.party;
+
+/**
+ * @author fengxin.fang
+ */
+public class StereoOffCommand implements Command {
+    Stereo stereo;
+
+    public StereoOffCommand(Stereo stereo) {
+        this.stereo = stereo;
+    }
+
+    @Override
+    public void execute() {
+        stereo.off();
+    }
+
+    @Override
+    public void undo() {
+        stereo.on();
+    }
+}
