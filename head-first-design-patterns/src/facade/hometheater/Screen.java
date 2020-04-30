4b687e77df5d4a95ad6f2bd3a11ac7f63b99e1a6
flexia <fangfengxin98@163.com>
2020-04-30 23:04:30 +0800

    Head First Design Patterns - Facade Pattern add hometheater classes

---
+++ head-first-design-patterns/src/facade/hometheater/Screen.java
@@ -0,0 +1,25 @@
+package facade.hometheater;
+
+/**
+ * @author fengxin.fang
+ */
+public class Screen {
+    String description;
+
+    public Screen(String description) {
+        this.description = description;
+    }
+
+    public void up() {
+        System.out.println(description + " going up");
+    }
+
+    public void down() {
+        System.out.println(description + " going down");
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
